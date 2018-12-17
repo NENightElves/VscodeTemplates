98d7aaa1d11cfd35b41b9d7cda07cca00f548647
NENightElves <956504045@qq.com>
2018-12-18 00:57:41 +0800

    add some templates

---
+++ hibernate/src/main/java/com/example/Test.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Test
+{
+    private Integer id;
+    private String a;
+
+	public Integer getId()
+	{
+		return this.id;
+	}
+
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+
+	public String getA()
+	{
+		return this.a;
+	}
+
+	public void setA(String a)
+	{
+		this.a = a;
+	}
+
+
+}
---
+++ struts2/src/main/java/TestAction.java
@@ -0,0 +1,9 @@
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+    @Override
+    public String execute() throws Exception{
+        System.out.println("测试");
+        return SUCCESS;
+    }
+}
